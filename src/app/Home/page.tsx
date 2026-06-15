bad3bb6681ed09c9fb268fa6893f26de83d8bb58
Pursharthzutshi <pursharthzutshi@gmail.com>
2026-06-16 02:52:33 +0530

    refactor: remove OnboardingGuide component from home page

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -10,7 +10,7 @@ import BoxWrapper from "./box-wrapper"
 import githubRepoAnalysis, { getLatestAnalysis } from "../(actions)/github-analysis"
 import "./home.css"
 import RepositoryGeneralOverview from "../../components/HomePage/RepositoryGeneralOverview"
-import OnboardingGuide from "../../components/HomePage/OnboardingGuide"
+
 
 export default function Home() {
 
